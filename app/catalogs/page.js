dfa49858f6abda366f1b5679aea556cce7addee1
alexeyShuncko <shuncko.a@yandex.ru>
2023-05-30 18:37:02 +0300

    catalogs

--- app/catalogs/page.js
+++ app/catalogs/page.js
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+import s from './page.module.css';
+import door1 from '../../public/door1.png';
+import door2 from '../../public/door2.png';
+import door3 from '../../public/door3.png';
+import door4 from '../../public/door4.png';
+
 export const metadata = {
   title: 'Каталоги | Двери Грузии',
 };
@@ -5,7 +12,33 @@ export const metadata = {
 export default function Catalogs() {
   return (
     <div>
-      <h1>Каталоги</h1>
+      <h1 className={s.title}>Каталоги</h1>
+      <ul className={s.listCollections}>
+        <li>
+          <a href="http://localhost:3000/catalog.pdf" download>
+            <Image src={door1} alt="Дверь" height={300} />
+            <h3>Коллекция 1</h3>
+          </a>
+        </li>
+        <li>
+          <a href="http://localhost:3000/catalog.pdf" download>
+            <Image src={door2} alt="Дверь" height={300} />
+            <h3>Коллекция 2</h3>
+          </a>
+        </li>
+        <li>
+          <a href="http://localhost:3000/catalog.pdf" download>
+            <Image src={door3} alt="Дверь" height={300} />
+            <h3>Коллекция 3</h3>
+          </a>
+        </li>
+        <li>
+          <a href="http://localhost:3000/catalog.pdf" download>
+            <Image src={door4} alt="Дверь" height={300} />
+            <h3>Коллекция 4</h3>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
