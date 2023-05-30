24e7c2caa3e8bc78a2084c510b96080809bb01b8
alexeyShuncko <shuncko.a@yandex.ru>
2023-05-30 19:13:17 +0300

    footer

--- components/MyFooter.js
+++ components/MyFooter.js
@@ -1,5 +1,52 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 const MyFooter = () => {
-  return <footer>&copy; Двери Грузии</footer>;
+  const pathname = usePathname();
+  return (
+    <footer>
+      <div className="lists">
+        <ul>
+          <li>
+            <Link href={'/'} className={pathname == '/' ? 'activeLink' : ''}>
+              Главная
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={'/catalogs'}
+              className={pathname === '/catalogs' ? 'activeLink' : ''}>
+              Каталоги
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={'/about'}
+              className={pathname === '/about' ? 'activeLink' : ''}>
+              О компании
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={'/contacts'}
+              className={pathname === '/contacts' ? 'activeLink' : ''}>
+              Контакты
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>+995-032-1234455</li>
+          <li>+995-032-1112233</li>
+          <li>dveri@mail.ru</li>
+          <li>topdverigruzii@mail.ru</li>
+          <li>Тбилиси, проспект Шота Руставели, 16</li>
+        </ul>
+      </div>
+      <div className="copy">&copy; Двери Грузии</div>
+    </footer>
+  );
 };
 
 export { MyFooter };
